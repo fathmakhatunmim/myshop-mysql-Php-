1cb647d339564961b9977806fe38eba0a042dfd8
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-28 16:50:20 +0600

    fist

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY shop</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>List of clients</h2>
+        <a class="btn btn-primary" href="/MYSQL_PHP/create.php" role="button">New Client</a>
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Create</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "myshop";
+
+                //create connection
+                $connection = new mysqli($servername, $username, $password, $database);
+
+                //check connection 
+                if ($connection -> connect_error) {
+                    die("connection failed: " . $connection -> connect_error);
+                }
+                //read all row from database table
+                $sql = "SELECT * FROM  clients";
+
+                $result = $connection-> query($sql);
+                if (!$result) {
+                    die("Invalid query: " . $connection->error);
+                }
+
+                //read data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo " <tr>
+                <td>$row[id]</td>
+                <td>$row[name]</td>
+                <td>$row[email]</td>
+                <td>$row[phone]</td>
+                <td>$row[address]</td>
+                <td>$row[create_at]</td>
+
+                <td>  
+                <a class='btn btn-primary btn-sm' href='/MYSQL_PHP/edit.php?id=$row[id]'>Edit</a>
+
+                <a class='btn btn-primary btn-sm' href='/MYSQL_PHP/edit.php?id=$row[id]'>Delete</a>
+
+                </td>
+              
+            </tr>";
+                }
+                ?>
+
+            </tbody>
+
+
+        </table>
+
+
+    </div>
+</body>
+
+</html>
